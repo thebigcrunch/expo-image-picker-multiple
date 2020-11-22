bfe1c5d577efe2f8376cd3a1bab0230c03a0804c
Simon Raik-Allen <shimmie@shimmie.com>
2020-11-23 10:52:58 +1100

    Added the dropzone for the webcomponent (rather than the expo-image-selector)

--- src/WebImageSelector.js
+++ src/WebImageSelector.js
@@ -4,40 +4,28 @@
 //
 
 import React from 'react';
+import { useDropzone } from 'react-dropzone';
 
-import { Button, Image, View, Platform } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-
-const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
+function MyDropzone() {
+    const onDrop = React.useCallback((acceptedFiles) => {
+        // Do something with the files
+    }, []);
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
     });
 
-    console.log(result);
-
-    if (!result.cancelled) {
-        setImage(result.uri);
-    }
-};
+    return (
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+                <p>Drop the files here ...</p>
+            ) : (
+                <p>Drag 'n' drop some files here, or click to select files</p>
+            )}
+        </div>
+    );
+}
 
 export const renderWebView = () => {
-    return (
-        <View
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-        >
-            <Button
-                title="Pick an image from camera roll"
-                onPress={pickImage}
-            />
-            {image && (
-                <Image
-                    source={{ uri: image }}
-                    style={{ width: 200, height: 200 }}
-                />
-            )}
-        </View>
-    );
+    return MyDropzone();
 };
